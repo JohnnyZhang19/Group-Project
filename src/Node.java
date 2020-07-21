78b652a989146e5ed3dbac12c940e1559854687c
elnganha <67536694+elnganha@users.noreply.github.com>
2020-07-21 13:15:48 -0600

    update node.java
    xCoord -> x
    yCoord -> y

--- src/Node.java
+++ src/Node.java
@@ -5,34 +5,34 @@
  *
  */
 public class Node {
-		private int xCoord;
-		private int yCoord;
+		private int x;
+		private int y;
 		private int type;
  
-		public Node(int xCoord, int yCoord, int type) {
-			this.xCoord = xCoord;
-			this.yCoord = yCoord;
+		public Node(int x, int y, int type) {
+			this.x = x;
+			this.y = y;
 			this.type = type;
 		}
 		
-		public int getXCoord() {
-			return xCoord;
+		public int getX() {
+			return x;
 		}
 		
-		public int getYCoord() {
-			return yCoord;
+		public int getY() {
+			return y;
 		}
 		
 		public int getType() {
 			return type;
 		}
 		
-		public void setXCoord(int xCoord) {
-			this.xCoord = xCoord;
+		public void setX(int x) {
+			this.x = x;
 		}
 		
-		public void setYCoord(int yCoord) {
-			this.yCoord = yCoord;
+		public void setY(int y) {
+			this.y = y;
 		}
 		
 		public void setType(int type) {
