13e3c9e6b43aa9b3008748c637b104faa02933b8
Zhezhee <yuzhe.zhou@ucalgary.ca>
2020-07-22 16:50:39 -0600

    Update the Obstacles class
    It doesn't finish yet

--- src/Obstacles.java
+++ src/Obstacles.java
@@ -4,15 +4,19 @@ public class Obstacles {
 	int obswidth = 3;
 	int obsheigt = 2;
 	Node loca;
-	Egg score;
 	
 	public Obstacles(Node location) {
 		this.loca = location;
 	}
 	
 	public void addObstacles() {
-		Random rand = new Random();
-		Node loca = new Node(rand.nextInt(15), rand.nextInt(15),2);
-		this.loca = loca;
+			Random rand = new Random();
+			Node loca = new Node(rand.nextInt(15), rand.nextInt(15),2);
+			this.loca = loca;
 	}
+	public Node getLocation() {
+		
+		return loca;
+	}
+	
 }
