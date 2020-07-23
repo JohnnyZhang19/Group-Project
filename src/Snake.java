ed611a7ab87c9d8d9b3bbaced31122fa9a4416f0
JohnnyZhang19 <68079378+JohnnyZhang19@users.noreply.github.com>
2020-07-22 18:48:11 -0600

    Update Snake.java

--- src/Snake.java
+++ src/Snake.java
@@ -1,72 +1,11 @@
+package snake;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.LinkedList;
+import java.util.LinkedList;
 
 public class Snake {
-	
 	private Node head;
 	private Node tail;
-	
-	// Create a List of type Node
-	// List<ClassName> listName;
-	private List<Node> body;
-	
-	
-	public Snake() {
-		body = new LinkedList<Node>();
-		
-		// By default there should be something in the snake body, hence we are adding one node. 
-		// Starting at 1,1 location.
-		Node node = new Node();
-		node.setxCoord(1);
-		node.setyCoord(1);
-		body.add(node);
-	}
-	public Node getTail() {
-		return tail;
-	}
-	public Node getHead() {
-		return head;
-	}
-	
-	public void addFromTail(Node node) {
-		body.add(node);
-	}
-	
-	public void addFromHead(Node node) {
-		//we can pass the index of the element where the node should be inserted
-		body.add(0,node);
-	}
-	
-	public void deleteFromTail() {
-		body.remove(body.size()-1);
-	}
-	
-	// If the snake touches the boundary. maxX maxY or lowX,lowY (0,0)
-	// Snake touches its own body
-	
-	public boolean checkDead(int maxX, int maxY) {
-		// Head is the first position in the body
-		head = body.get(0);
-
-		// tail is the last position in the body
-		tail = body.get(body.size()-1);
-		
-		
-		if(head.getxCoord()>=maxX || head.getyCoord()>=maxY || head.getxCoord()<0 || head.getyCoord()<0) {
-			return true;
-		}else {
-			
-			for(int i=1;i<body.size();i++) {
-				Node bodyNode = body.get(i);
-				if(bodyNode.getxCoord()==head.getxCoord() && bodyNode.getyCoord()==head.getyCoord()) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+	private LinkedList<Node> body = new LinkedList<>();
 	
 	public LinkedList<Node> getBody() {
 		return body;
@@ -74,5 +13,32 @@ public class Snake {
 	public void setBody(LinkedList<Node> body) {
 		this.body = body;
 	}
+	public Node getHead() {
+		return head;
+	}
+	public Node getTail() {
+		return tail;
+	}
+	public Snake(Node head, Node tail) {
+		this.head = head;
+		this.tail = tail;
+		body.add(head);
+		body.add(tail);
+	}
+	
+	public Snake() {}
+	
+	public void addFromHead(Node node) {
+		body.addFirst(node);
+	}
+	
+	public void addFromTail(Node node) {
+		body.addLast(node);
+	}
+	
+	public void deleteFromTail(Node node) {
+		body.removeLast();
+	}
+
 	
 }
