a4b062579351813a5aa491d8aba3f311161993b6
elnganha <67536694+elnganha@users.noreply.github.com>
2020-08-03 12:46:41 -0600

    Select level for Snake. No speed setting yet.

--- src/snake/Main.java
+++ src/snake/Main.java
@@ -4,14 +4,17 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ComboBoxBase;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -32,11 +35,26 @@ public class Main extends Application{
 		Snake newSnake = new Snake();
 		ObservableList<String> options = 
 			    FXCollections.observableArrayList(
-			        "Option 1",
-			        "Option 2",
-			        "Option 3"
+			        "Easy",
+			        "Normal",
+			        "Hard"
 			    );
 		root.getChildren().addAll(new Label("Select level"), new ComboBox(options));
+		EventHandler<ActionEvent> event = 
+                new EventHandler<ActionEvent>() { 
+          public void handle(ActionEvent e) 
+          { 
+              if(ComboBox(options).getValue().equals("Easy")) {
+            	  //Speed of snake is slow
+              }; 
+              if(ComboBox(options).getValue().equals("Normal")) {
+            	  //Speed of snake is normal
+              }; 
+              if(ComboBox(options).getValue().equals("Hard")) {
+            	  //Speed of snake is fast
+              }; 
+          }
+
 
 		
 		
