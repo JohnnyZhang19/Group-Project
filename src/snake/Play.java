a14a6efbd2393424d0e756b136a924785f2142b9
elnganha <67536694+elnganha@users.noreply.github.com>
2020-08-02 23:35:46 -0600

    Play.java

---
+++ src/snake/Play.java
@@ -0,0 +1,32 @@
+package snake;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Play extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		VBox root = new VBox();
+		
+		
+	}
+
+	
+		
+	}
+
+	
